5c73402421df0cfdb8c7c27167156b029af25402
rlakhani21 <96112664+rlakhani21@users.noreply.github.com>
2024-03-19 11:35:02 +0530

    Add files via upload

--- app/dfg/ViewController.swift
+++ app/dfg/ViewController.swift
@@ -8,11 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var myBookButton: UIButton!
+
+    @IBOutlet weak var myBooksButton: UITextField!
+    
+    @IBOutlet weak var pickview: UIPickerView!
+    
+
+    func numberOfComponents(in pickerview: UIPickerView) -> Int {
+        return 1 ;
+    }
+    
+  
+    @IBAction func mybookbutton(_ sender: Any) {
+        myBooksButton.becomeFirstResponder()
+
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+     
     }
+    
 
 
 }
